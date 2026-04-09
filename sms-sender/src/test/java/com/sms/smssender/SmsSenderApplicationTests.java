ed9696c66fcb4805a1a256e7d9594b82abdbf4ee
Mansi Kulkarni <mansiak1234@gmail.com>
2026-04-10 00:43:59 +0530

    add unit tests for sms sender

--- sms-sender/src/test/java/com/sms/smssender/SmsSenderApplicationTests.java
+++ sms-sender/src/test/java/com/sms/smssender/SmsSenderApplicationTests.java
@@ -1,13 +1,48 @@
 package com.sms.smssender;
 
+import com.sms.smssender.model.SmsRequest;
+import com.sms.smssender.model.SmsEvent;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-class SmsSenderApplicationTests {
+public class SmsSenderApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testSmsRequest_GettersSetters() {
+		SmsRequest request = new SmsRequest();
+		request.setPhoneNumber("9876543210");
+		request.setMessage("Hello!");
+
+		assertEquals("9876543210", request.getPhoneNumber());
+		assertEquals("Hello!", request.getMessage());
 	}
 
+	@Test
+	void testSmsRequest_Constructor() {
+		SmsRequest request = new SmsRequest("9876543210", "Hello!");
+
+		assertEquals("9876543210", request.getPhoneNumber());
+		assertEquals("Hello!", request.getMessage());
+	}
+
+	@Test
+	void testSmsEvent_Constructor() {
+		SmsEvent event = new SmsEvent("9876543210", "Hello!", "SUCCESS");
+
+		assertEquals("9876543210", event.getPhoneNumber());
+		assertEquals("Hello!", event.getMessage());
+		assertEquals("SUCCESS", event.getStatus());
+	}
+
+	@Test
+	void testSmsEvent_GettersSetters() {
+		SmsEvent event = new SmsEvent();
+		event.setPhoneNumber("9876543210");
+		event.setMessage("Hello!");
+		event.setStatus("SUCCESS");
+
+		assertEquals("9876543210", event.getPhoneNumber());
+		assertEquals("Hello!", event.getMessage());
+		assertEquals("SUCCESS", event.getStatus());
+	}
 }
